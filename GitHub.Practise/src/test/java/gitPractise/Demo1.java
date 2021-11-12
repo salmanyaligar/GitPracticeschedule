dd384de18b08a1f23920c48e7e5fb78d01ab2c6d
Techway <Techway@DESKTOP-OBO2JJ9>
2021-11-12 15:58:36 +0530

    First update

---
+++ GitHub.Practise/src/test/java/gitPractise/Demo1.java
@@ -0,0 +1,15 @@
+package gitPractise;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void creat_test()
+	{
+		System.out.println("user 1 my first commit");
+		
+		
+		System.out.println("user 1 updated by reddy");
+	}
+}
---
+++ GitHub.Practise/src/test/java/gitPractise/Demo2.java
@@ -0,0 +1,13 @@
+package gitPractise;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+	@Test
+	public void push()
+	{
+		System.out.println("user 1 2nd commit");
+		
+	}
+}
