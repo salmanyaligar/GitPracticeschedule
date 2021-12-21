fcae18e944904f3d0b71ce3854651e2a0898f270
salmanyaligar <salmanyaligar@gmail.com>
2021-12-21 15:49:07 +0530

    updated

---
+++ GitHub.Practise/src/test/java/gitPractise/Chandan.java
@@ -0,0 +1,13 @@
+package gitPractise;
+
+import org.testng.annotations.Test;
+
+public class Chandan {
+	@Test
+public void Salman() {
+		String URL = System.getProperty("url");
+		String Browser = System.getProperty("browser");
+		
+	System.out.println(Browser+"  "+URL);
+}
+}
